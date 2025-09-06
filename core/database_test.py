0897d22e912f78528368d4ded72f64592952b675
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-09-06 12:59:18 +0330

    in core/database_test.py refactoring User model class and write how to do CRUD operations on database with SQLARCHEMY .

--- core/database_test.py
+++ core/database_test.py
@@ -2,7 +2,8 @@ from sqlalchemy import (
     create_engine,
     Column,
     Integer,
-    String
+    String,
+    Boolean
 )
 from sqlalchemy.orm import (
     sessionmaker,
@@ -35,10 +36,46 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String(length=30))
-    last_name = Column(String(length=30))
+    last_name = Column(String(length=30), nullable=True)
+    age = Column(Integer)
+    is_active = Column(Boolean, default=True)
+    is_verified = Column(Boolean, default=False)
     
-    def __repre__(self):
+    def __repr__(self):
         return f"User(id={self.id}, first_name={self.first_name}, last_name={self.last_name})"
 
 # to create tables and database
 Base.metadata.create_all(engine)
+
+session = SessionLocal()
+
+# Inserting adata(create)
+# hamed = User(first_name="mohsen", last_name="golriz", age=34)
+# session.add(hamed)
+# session.commit()
+
+# bulk insert(bulk_create)
+# mohsen = User(first_name="mohsen", last_name="golriz", age=34)
+# zahra = User(first_name="zahra", last_name="fadai", age=23)
+# session.add_all([mohsen, zahra])
+# session.commit()
+
+# retrieve all data(get-list)
+# users = session.query(User).all()
+# print(type(users), users)
+# print([user.age for user in users])
+
+# retrieve data(get-detail)
+# user = session.query(User).filter_by(first_name="hamed").first()
+# print(user, type(user))
+
+# updating a record of data(PUT/PATCH)
+# user.last_name = "gholizade"
+# session.commit()
+
+# delete a record of data(DELETE)
+# if user:
+#     session.delete(user)
+#     session.commit()
+# else:
+#     print("user does not exist")
